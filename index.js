942a05541c2d6ab6a3b63e27544a92ede49140f1
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 19:13:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ function iterativeLog(array) {
   });
 }
 
-// Define a function, iterate, that accepts a callback. Within the iterate() function, you should initialize an array. It can contain anything you want, but make sure that it is not empty! Call .forEach() on this array, passing the callback to .forEach(). Then return the array that you initialized.
+// Define a function, iterate, that accepts a callback.
+// Within the iterate() function, you should initialize an array.
+// It can contain anything you want, but make sure that it is not empty! 
+// Call .forEach() on this array, passing the callback to .forEach(). 
+// Then return the array that you initialized.
+
+function iterate() {
+  
+}
 // Define a function, doToArray that accepts an array and a callback. Call .forEach() on the array, passing the callback as the forEach callback.
