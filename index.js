8aae956387aad719dd2457ffb01db41c643bef41
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 19:02:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 // Call .forEach() on this array, and inside the callback, log each element
 // with the format ${index}: ${element}.
 function iterativeLog(array) {
-  array.forEach(console.log())
+  array.forEach(function()
+    console.log())
 }
 
 // Define a function, iterate, that accepts a callback. Within the iterate() function, you should initialize an array. It can contain anything you want, but make sure that it is not empty! Call .forEach() on this array, passing the callback to .forEach(). Then return the array that you initialized.
