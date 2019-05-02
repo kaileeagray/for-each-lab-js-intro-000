888479bf3eb1facc20745c6e043dad593f86c20a
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 19:15:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function iterate(callback) {
   kids.forEach(callback);
   return kids;
 }
-// Define a function, doToArray that accepts an array and a callback. 
+// Define a function, doToArray that accepts an array and a callback.
 // Call .forEach() on the array, passing the callback as the forEach callback.
